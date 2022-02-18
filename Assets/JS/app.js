4e4eace86d8b368e4a4669f54408b30dcffefd56
Joydip Paul <joydippaul72@gmail.com>
2022-02-19 03:23:53 +0600

    cakery nav footer add hoise

---
+++ Assets/JS/app.js
@@ -0,0 +1,34 @@
+    if ($("#sidemenu_toggle").length) {
+        $("#sidemenu_toggle").on("click", function () {
+            $(".pushwrap").toggleClass("active");
+            $(".side-menu").addClass("side-menu-active"), $("#close_side_menu").fadeIn(700)
+        }), $("#close_side_menu").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $(this).fadeOut(200), $(".pushwrap").removeClass("active")
+        // }), $(".side-nav .navbar-nav .nav-link").on("click", function () {
+        //     $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+        }), $("#btn_sideNavClose").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+        });
+    }
+
+
+// sticky navbar
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+}); 
+
+// Date Get
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById("year").innerHTML = year;
