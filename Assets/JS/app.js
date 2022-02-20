4a21744dd2e36630b791efe7e4b774686e8d236c
Joydip Paul <joydippaul72@gmail.com>
2022-02-21 05:56:57 +0600

    onek kisu

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -32,3 +32,22 @@ document.addEventListener("DOMContentLoaded", function () {
 const date = new Date();
 const year = date.getFullYear();
 document.getElementById("year").innerHTML = year;
+
+// Carousel 
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
